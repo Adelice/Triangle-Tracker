3e0022bc922805b6c0ff82b655c9915105614a8f
Adelice <nancytoniw@gmail.com>
2018-12-14 15:35:17 +0200

    add a background image

--- triangle-tracker.js
+++ triangle-tracker.js
@@ -1,5 +1,9 @@
-function myTriangle(){
-    var sideOne=document.getElementById("nber").value;
-    var sideTwo=document.getElementById("nber1").value;
-    var sideThree=document.getElementById("nber2").value;
+function triangle(){
+    var sideOne=document.getElementById("side1").value;
+    var sideTwo=document.getElementById("side2").value;
+    var sideThree=document.getElementById("side3").value;
+
+if(sideOne === '' || sideTwo === '' || sideThree === '') {
+    alert("Please enter three side lengths");
+}
 }
