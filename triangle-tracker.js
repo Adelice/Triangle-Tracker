7c589c68c3aa63006f793a9bd66ac8e3682a8629
Adelice <nancytoniw@gmail.com>
2018-12-17 18:42:49 +0200

    isosceles, scalene

--- triangle-tracker.js
+++ triangle-tracker.js
@@ -1,26 +1,26 @@
 function triangle(){
-    var sideOne=document.getElementById("side1").value;
-    var sideTwo=document.getElementById("side2").value;
-    var sideThree=document.getElementById("side3").value;
-
-if(
-    sideOne===sideTwo  && sideTwo===sideThree) {
-        alert("This is an equilateral Triangle");
-        }
-        
-       // else if (sideOne===sideTwo || sideTwo===sideThree || sideOne===sideThree ) {
-       // alert("This is an isosceles Triangle");
-        }
-        
-        //else if ( sideOne+sideTwo>sideThree || sideTwo+sideThree>=sideOne || sideOne+sideThree<=sideTwo ) {
-       // alert("This is an scalene Triangle");
-        //}
-
-
-if(sideOne === '' || sideTwo === '' || sideThree === '') {
-        alert("Please enter three side lengths");
-
-}
+   var sideone = document.getElementById("side1").value;
+   var sidetwo = document.getElementById("side2").value;
+   var sidethree = document.getElementById("side3").value;
+   var array=['equilateral','isocseles','scalene'];
   
-
+  if (sideone <= 0 || sidetwo <= 0 || sidethree <= 0)  {
+      alert("Please enter valid sides");
+  }
+   else if((sideone===sidetwo) && (sidetwo===sidethree) && (sidethree===sideone)) {
+    alert("This is an equilateral Triangle");
+   }
+   else if (sideone === sidetwo || sidetwo===sidethree|| sideone ===sidethree ){
+    alert("This is an isosceles Triangle");
+   }
+  
+   else if((sideone + sidetwo >= sidethree &&sidetwo + sideone >= sidethree) ||
+(sideone + sidetwo>= sidethree && sidethree + sideone >= sidetwo) || (sidetwo + sidethree >= sideone && sidethree + sidetwo >= sideone))
+   {
+         alert("this is a scalene triangle");
+   }
+   else{
+       alert("N0T A TRIANGLE");
+   }
+}
 
