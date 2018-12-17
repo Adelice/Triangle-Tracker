d3c399f81fadde6a4798c96e33e90b67e2da722a
Adelice <nancytoniw@gmail.com>
2018-12-17 14:47:29 +0200

    equilateral triangle

--- triangle-tracker.js
+++ triangle-tracker.js
@@ -3,9 +3,24 @@ function triangle(){
     var sideTwo=document.getElementById("side2").value;
     var sideThree=document.getElementById("side3").value;
 
+if(
+    sideOne===sideTwo  && sideTwo===sideThree) {
+        alert("This is an equilateral Triangle");
+        }
+        
+       // else if (sideOne===sideTwo || sideTwo===sideThree || sideOne===sideThree ) {
+       // alert("This is an isosceles Triangle");
+        }
+        
+        //else if ( sideOne+sideTwo>sideThree || sideTwo+sideThree>=sideOne || sideOne+sideThree<=sideTwo ) {
+       // alert("This is an scalene Triangle");
+        //}
+
+
 if(sideOne === '' || sideTwo === '' || sideThree === '') {
-    alert("Please enter three side lengths");
-}else if{
-    
+        alert("Please enter three side lengths");
+
 }
-}
+  
+
+
